svn2github/pjproject

commit 66a1d0cd85f7026d1842717f294abba1188c8799

More ticket #507: updated GNU build system with the G.722 codec and fixed minor warning about operator precedence
git-svn-id: http://svn.pjsip.org/repos/pjproject@1871 74dad513-b988-da41-8d7b-12977e46ad98

trunk/pjmedia/src/pjmedia-codec/g722/g722_enc.c
@@ -76,7 +76,7 @@ static int block1l (int xl, int sl, int detl)
 
     sil = el >> 15 ;
     if (sil == 0 )  wd = el ;
-    else wd = 32767 - el & 32767 ;
+    else wd = 32767 - (el & 32767) ;
 
     mil = 1 ;
 
@@ -304,7 +304,7 @@ static int block1h (int xh, int sh, int deth)
 
     sih = eh >> 15 ;
     if (sih == 0 )  wd = eh ;
-    else wd = 32767 - eh & 32767 ;
+    else wd = 32767 - (eh & 32767) ;
 
     hdu = (564 << 3) * deth;
     wd1 = (hdu >> 15) ;

trunk/aconfigure
@@ -308,7 +308,7 @@ ac_includes_default="\
 # include <unistd.h>
 #endif"
 
-ac_subst_vars='SHELL PATH_SEPARATOR PACKAGE_NAME PACKAGE_TARNAME PACKAGE_VERSION PACKAGE_STRING PACKAGE_BUGREPORT exec_prefix prefix program_transform_name bindir sbindir libexecdir datadir sysconfdir sharedstatedir localstatedir libdir includedir oldincludedir infodir mandir build_alias host_alias target_alias DEFS ECHO_C ECHO_N ECHO_T LIBS build build_cpu build_vendor build_os host host_cpu host_vendor host_os target target_cpu target_vendor target_os CC CFLAGS LDFLAGS CPPFLAGS ac_ct_CC EXEEXT OBJEXT CXX CXXFLAGS ac_ct_CXX ac_pjdir CPP EGREP ac_os_objs ac_pjmedia_snd ac_pa_cflags ac_pa_use_alsa ac_pa_use_oss ac_no_small_filter ac_no_large_filter ac_no_speex_aec ac_no_g711_codec ac_no_l16_codec ac_no_gsm_codec ac_no_speex_codec ac_no_ilbc_codec ac_no_ssl openssl_h_present libssl_present libcrypto_present ac_cross_compile ac_linux_poll ac_host ac_main_obj LIBOBJS LTLIBOBJS'
+ac_subst_vars='SHELL PATH_SEPARATOR PACKAGE_NAME PACKAGE_TARNAME PACKAGE_VERSION PACKAGE_STRING PACKAGE_BUGREPORT exec_prefix prefix program_transform_name bindir sbindir libexecdir datadir sysconfdir sharedstatedir localstatedir libdir includedir oldincludedir infodir mandir build_alias host_alias target_alias DEFS ECHO_C ECHO_N ECHO_T LIBS build build_cpu build_vendor build_os host host_cpu host_vendor host_os target target_cpu target_vendor target_os CC CFLAGS LDFLAGS CPPFLAGS ac_ct_CC EXEEXT OBJEXT CXX CXXFLAGS ac_ct_CXX ac_pjdir CPP EGREP ac_os_objs ac_pjmedia_snd ac_pa_cflags ac_pa_use_alsa ac_pa_use_oss ac_no_small_filter ac_no_large_filter ac_no_speex_aec ac_no_g711_codec ac_no_l16_codec ac_no_gsm_codec ac_no_g722_codec ac_no_speex_codec ac_no_ilbc_codec ac_no_ssl openssl_h_present libssl_present libcrypto_present ac_cross_compile ac_linux_poll ac_host ac_main_obj LIBOBJS LTLIBOBJS'
 ac_subst_files=''
 
 # Initialize some variables set by options.
@@ -865,6 +865,7 @@ Optional Features:
   --disable-g711-codec    Exclude G.711 codecs from the build
   --disable-l16-codec     Exclude Linear/L16 codec family from the build
   --disable-gsm-codec     Exclude GSM codec in the build
+  --disable-g722-codec    Exclude G.722 codec in the build
   --disable-speex-codec   Exclude Speex codecs in the build
   --disable-ilbc-codec    Exclude iLBC codec in the build
   --enable-libsamplerate  Link with libsamplerate when available. Note that
@@ -10474,6 +10475,24 @@ echo "${ECHO_T}Checking if GSM codec is disabled...no" >&6
 fi;
 
 
+# Check whether --enable-g722-codec or --disable-g722-codec was given.
+if test "${enable_g722_codec+set}" = set; then
+  enableval="$enable_g722_codec"
+  if test "$enable_g722_codec" = "no"; then
+		ac_no_g722_codec=1
+		cat >>confdefs.h <<\_ACEOF
+#define PJMEDIA_HAS_G722_CODEC 0
+_ACEOF
+
+		echo "$as_me:$LINENO: result: Checking if G.722 codec is disabled...yes" >&5
+echo "${ECHO_T}Checking if G.722 codec is disabled...yes" >&6
+	       fi
+else
+  echo "$as_me:$LINENO: result: Checking if G.722 codec is disabled...no" >&5
+echo "${ECHO_T}Checking if G.722 codec is disabled...no" >&6
+fi;
+
+
 # Check whether --enable-speex-codec or --disable-speex-codec was given.
 if test "${enable_speex_codec+set}" = set; then
   enableval="$enable_speex_codec"
@@ -11677,6 +11696,7 @@ s,@ac_no_speex_aec@,$ac_no_speex_aec,;t t
 s,@ac_no_g711_codec@,$ac_no_g711_codec,;t t
 s,@ac_no_l16_codec@,$ac_no_l16_codec,;t t
 s,@ac_no_gsm_codec@,$ac_no_gsm_codec,;t t
+s,@ac_no_g722_codec@,$ac_no_g722_codec,;t t
 s,@ac_no_speex_codec@,$ac_no_speex_codec,;t t
 s,@ac_no_ilbc_codec@,$ac_no_ilbc_codec,;t t
 s,@ac_no_ssl@,$ac_no_ssl,;t t

trunk/aconfigure.ac
@@ -475,6 +475,18 @@ AC_ARG_ENABLE(gsm-codec,
 	       fi],
 	      AC_MSG_RESULT([Checking if GSM codec is disabled...no]))
 
+dnl # Include G.722 codec
+AC_SUBST(ac_no_g722_codec)
+AC_ARG_ENABLE(g722-codec,
+	      AC_HELP_STRING([--disable-g722-codec],
+			     [Exclude G.722 codec in the build]),
+	      [if test "$enable_g722_codec" = "no"; then
+		[ac_no_g722_codec=1]
+		AC_DEFINE(PJMEDIA_HAS_G722_CODEC,0)
+		AC_MSG_RESULT([Checking if G.722 codec is disabled...yes])
+	       fi],
+	      AC_MSG_RESULT([Checking if G.722 codec is disabled...no]))
+
 dnl # Include Speex codec
 AC_SUBST(ac_no_speex_codec)
 AC_ARG_ENABLE(speex-codec,

trunk/pjmedia/build/os-auto.mak.in
@@ -26,6 +26,7 @@ AC_NO_L16_CODEC=@ac_no_l16_codec@
 AC_NO_GSM_CODEC=@ac_no_gsm_codec@
 AC_NO_SPEEX_CODEC=@ac_no_speex_codec@
 AC_NO_ILBC_CODEC=@ac_no_ilbc_codec@
+AC_NO_G722_CODEC=@ac_no_g722_codec@
 
 export CODEC_OBJS=
 
@@ -63,6 +64,12 @@ ifeq ($(AC_NO_ILBC_CODEC),1)
 export CFLAGS += -DPJMEDIA_HAS_ILBC_CODEC=0
 else
 export CODEC_OBJS += ilbc.o
+endif
+
+ifeq ($(AC_NO_G722_CODEC),1)
+export CFLAGS += -DPJMEDIA_HAS_G722_CODEC=0
+else
+export CODEC_OBJS += g722.o g722/g722_enc.o g722/g722_dec.o
 endif
 
 

trunk/pjmedia/build/os-darwinos.mak
@@ -23,6 +23,7 @@ AC_NO_L16_CODEC=0
 AC_NO_GSM_CODEC=0
 AC_NO_SPEEX_CODEC=0
 AC_NO_ILBC_CODEC=0
+AC_NO_G722_CODEC=0
 
 export CODEC_OBJS=
 
@@ -61,6 +62,12 @@ ifeq ($(AC_NO_ILBC_CODEC),1)
 export CFLAGS += -DPJMEDIA_HAS_ILBC_CODEC=0
 else
 export CODEC_OBJS += ilbc.o
+endif
+
+ifeq ($(AC_NO_G722_CODEC),1)
+export CFLAGS += -DPJMEDIA_HAS_G722_CODEC=0
+else
+export CODEC_OBJS += g722.o g722/g722_enc.o g722/g722_dec.o
 endif
 
 

trunk/pjmedia/build/os-linux.mak
@@ -18,6 +18,7 @@ AC_NO_L16_CODEC=0
 AC_NO_GSM_CODEC=0
 AC_NO_SPEEX_CODEC=0
 AC_NO_ILBC_CODEC=0
+AC_NO_G722_CODEC=0
 
 export CODEC_OBJS=
 
@@ -55,6 +56,12 @@ ifeq ($(AC_NO_ILBC_CODEC),1)
 export CFLAGS += -DPJMEDIA_HAS_ILBC_CODEC=0
 else
 export CODEC_OBJS += ilbc.o
+endif
+
+ifeq ($(AC_NO_G722_CODEC),1)
+export CFLAGS += -DPJMEDIA_HAS_G722_CODEC=0
+else
+export CODEC_OBJS += g722.o g722/g722_enc.o g722/g722_dec.o
 endif
 
 

trunk/pjmedia/build/os-win32.mak
@@ -19,6 +19,7 @@ AC_NO_L16_CODEC=0
 AC_NO_GSM_CODEC=0
 AC_NO_SPEEX_CODEC=0
 AC_NO_ILBC_CODEC=0
+AC_NO_G722_CODEC=0
 
 export CODEC_OBJS=
 
@@ -57,6 +58,12 @@ ifeq ($(AC_NO_ILBC_CODEC),1)
 export CFLAGS += -DPJMEDIA_HAS_ILBC_CODEC=0
 else
 export CODEC_OBJS += ilbc.o
+endif
+
+ifeq ($(AC_NO_G722_CODEC),1)
+export CFLAGS += -DPJMEDIA_HAS_G722_CODEC=0
+else
+export CODEC_OBJS += g722.o g722/g722_enc.o g722/g722_dec.o
 endif
 
 

trunk/pjmedia/include/pjmedia-codec/config_auto.h.in
@@ -54,6 +54,12 @@
 #endif
 
 
+/* G722 codec */
+#ifndef PJMEDIA_HAS_G722_CODEC
+#undef PJMEDIA_HAS_G722_CODEC
+#endif
+
+
 #endif	/* __PJMEDIA_CODEC_CONFIG_AUTO_H_ */
 
 

commit 0b9e50b6a58e40bdb981106aa4ee71f1fb3cb238

More ticket #507: fixed the bug introduced in r1871 about operator precedence
git-svn-id: http://svn.pjsip.org/repos/pjproject@1872 74dad513-b988-da41-8d7b-12977e46ad98

trunk/pjmedia/src/pjmedia-codec/g722/g722_enc.c
@@ -76,7 +76,7 @@ static int block1l (int xl, int sl, int detl)
 
     sil = el >> 15 ;
     if (sil == 0 )  wd = el ;
-    else wd = 32767 - (el & 32767) ;
+    else wd = (32767 - el) & 32767 ;
 
     mil = 1 ;
 
@@ -304,7 +304,7 @@ static int block1h (int xh, int sh, int deth)
 
     sih = eh >> 15 ;
     if (sih == 0 )  wd = eh ;
-    else wd = 32767 - (eh & 32767) ;
+    else wd = (32767 - eh) & 32767 ;
 
     hdu = (564 << 3) * deth;
     wd1 = (hdu >> 15) ;